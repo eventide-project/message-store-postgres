51459cf383d65d747c9be4b40100f3608e7a1777
Scott Bellware <sbellware@gmail.com>
2016-10-07 19:53:13 -0500

    Log replaces TElemetry::Logger

--- lib/event_source/postgres/iterator.rb
+++ lib/event_source/postgres/iterator.rb
@@ -3,6 +3,8 @@ module EventSource
     class Iterator
       class Error < RuntimeError; end
 
+      include Log::Dependency
+
       attr_accessor :batch
       attr_writer :batch_position
       attr_writer :stream_offset
@@ -17,15 +19,13 @@ module EventSource
 
       dependency :get, Get
       dependency :cycle, Cycle
-      dependency :logger, Telemetry::Logger
 
       initializer :stream_name, :category, a(:stream_position, 0), :batch_size, :precedence
 
       def self.build(stream_name: nil, category: nil, stream_position: nil, batch_size: nil, precedence: nil, delay_milliseconds: nil, timeout_milliseconds: nil, cycle: nil, session: nil)
         new(stream_name, category, stream_position, batch_size, precedence).tap do |instance|
           Get.configure instance, stream_name: stream_name, category: category, batch_size: batch_size, precedence: precedence, session: session
-          Telemetry::Logger.configure instance
-          Iterator::Cycle.configure instance, delay_milliseconds: delay_milliseconds, timeout_milliseconds: timeout_milliseconds, cycle: cycle
+          Cycle.configure instance, delay_milliseconds: delay_milliseconds, timeout_milliseconds: timeout_milliseconds, cycle: cycle
         end
       end
 
@@ -36,14 +36,14 @@ module EventSource
       end
 
       def next
-        logger.opt_trace "Getting next event data (Batch Length: #{batch.nil? ? '<none>' : batch.length}, Batch Position: #{batch_position}, Stream Offset: #{stream_offset})"
+        logger.trace "Getting next event data (Batch Length: #{batch.nil? ? '<none>' : batch.length}, Batch Position: #{batch_position}, Stream Offset: #{stream_offset})"
 
         resupply(batch)
 
         event_data = batch[batch_position]
 
-        logger.opt_debug "Finished getting next event data (Batch Length: #{batch.nil? ? '<none>' : batch.length}, Batch Position: #{batch_position}, Stream Offset: #{stream_offset})"
-        logger.opt_data "Event Data: #{event_data.inspect}"
+        logger.debug "Finished getting next event data (Batch Length: #{batch.nil? ? '<none>' : batch.length}, Batch Position: #{batch_position}, Stream Offset: #{stream_offset})"
+        logger.data "Event Data: #{event_data.inspect}"
 
         advance_positions
 
@@ -51,7 +51,7 @@ module EventSource
       end
 
       def resupply(batch)
-        logger.opt_trace "Resupplying batch"
+        logger.trace "Resupplying batch"
 
         if batch.nil?
           batch_log_text = "Batch: #{batch.inspect}"
@@ -68,38 +68,38 @@ module EventSource
           logger.debug "Current batch not depleted (#{batch_log_text}, Batch Position: #{batch_position})"
         end
 
-        logger.opt_debug "Finished resupplying batch"
+        logger.debug "Finished resupplying batch"
       end
 
       def reset(batch)
-        logger.opt_trace "Resetting batch"
+        logger.trace "Resetting batch"
 
         self.batch = batch
         self.batch_position = 0
 
-        logger.opt_debug "Reset batch"
-        logger.opt_data ("Batch: #{batch.inspect}")
-        logger.opt_data ("Batch Position: #{batch_position.inspect}")
+        logger.debug "Reset batch"
+        logger.data ("Batch: #{batch.inspect}")
+        logger.data ("Batch Position: #{batch_position.inspect}")
       end
 
       def get_batch
-        logger.opt_trace "Getting batch"
+        logger.trace "Getting batch"
 
         batch = nil
         cycle.() do
           batch = get.(stream_position: stream_offset)
         end
 
-        logger.opt_debug "Finished getting batch (Count: #{batch.length})"
+        logger.debug "Finished getting batch (Count: #{batch.length})"
 
         batch
       end
 
       def advance_positions
-        logger.opt_trace "Advancing positions (Batch Position: #{batch_position}, Stream Offset: #{stream_offset})"
+        logger.trace "Advancing positions (Batch Position: #{batch_position}, Stream Offset: #{stream_offset})"
         self.batch_position += 1
         self.stream_offset += 1
-        logger.opt_debug "Advanced positions (Batch Position: #{batch_position}, Stream Offset: #{stream_offset})"
+        logger.debug "Advanced positions (Batch Position: #{batch_position}, Stream Offset: #{stream_offset})"
       end
     end
   end
--- test/test_init.rb
+++ test/test_init.rb
@@ -1,14 +1,5 @@
 ENV['CONSOLE_DEVICE'] ||= 'stdout'
-ENV['LOG_COLOR'] ||= 'on'
-
-if ENV['LOG_LEVEL']
-  ENV['LOGGER'] ||= 'on'
-else
-  ENV['LOG_LEVEL'] ||= 'trace'
-end
-
-ENV['LOGGER'] ||= 'off'
-ENV['LOG_OPTIONAL'] ||= 'on'
+ENV['LOG_LEVEL'] ||= 'fatal'
 
 puts RUBY_DESCRIPTION
 
@@ -17,7 +8,6 @@ require 'event_source/postgres/controls'
 
 require 'test_bench'; TestBench.activate
 
-Telemetry::Logger::AdHoc.activate
 require 'pp'
 
 include EventSource
