fce33cdfa5b8d8acfe84c792769a01bacb5f1a88
Scott Bellware <sbellware@gmail.com>
2016-10-23 21:05:51 -0500

    Test coverage of starting position

---
+++ test/automated/read/starting_position.rb
@@ -0,0 +1,17 @@
+require_relative '../automated_init'
+
+context "Read" do
+  context "Starting Position" do
+    stream = Controls::Put.(instances: 2)
+
+    batch = []
+
+    Read.(stream.name, stream_position: 1, batch_size: 1) do |event_data|
+      batch << event_data
+    end
+
+    test "Reads from the starting position" do
+      assert(batch.length == 1)
+    end
+  end
+end
