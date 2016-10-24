be5510b9ee0e234971baa25cdda4f76748e4dd83
Scott Bellware <sbellware@gmail.com>
2016-10-23 20:29:08 -0500

    Read from partition

--- lib/event_source/postgres/iterator.rb
+++ lib/event_source/postgres/iterator.rb
@@ -20,18 +20,19 @@ module EventSource
       dependency :get, Get
       dependency :cycle, Cycle
 
-      initializer :stream, a(:stream_position, 0), :batch_size, :precedence
+      ## TODO remove a() with default
+      initializer :stream, a(:stream_position, 0), :batch_size, :precedence, :partition
 
-      def self.build(stream, stream_position: nil, batch_size: nil, precedence: nil, cycle: nil, session: nil)
-        new(stream, stream_position, batch_size, precedence).tap do |instance|
-          Get.configure instance, stream, batch_size: batch_size, precedence: precedence, session: session
+      def self.build(stream, stream_position: nil, batch_size: nil, precedence: nil, partition: nil, cycle: nil, session: nil)
+        new(stream, stream_position, batch_size, precedence, partition).tap do |instance|
+          Get.configure instance, stream, batch_size: batch_size, precedence: precedence, partition: partition, session: session
           Cycle.configure instance, cycle: cycle
         end
       end
 
-      def self.configure(receiver, stream,  attr_name: nil, stream_position: nil, batch_size: nil, precedence: nil, cycle: nil, session: nil)
+      def self.configure(receiver, stream, attr_name: nil, stream_position: nil, batch_size: nil, precedence: nil, partition: nil, cycle: nil, session: nil)
         attr_name ||= :iterator
-        instance = build(stream, stream_position: stream_position, batch_size: batch_size, precedence: precedence, cycle: cycle, session: session)
+        instance = build(stream, stream_position: stream_position, batch_size: batch_size, precedence: precedence, partition: partition, cycle: cycle, session: session)
         receiver.public_send "#{attr_name}=", instance
       end
 
---
+++ test/automated/read/partition.rb
@@ -0,0 +1,17 @@
+require_relative '../automated_init'
+
+context "Read" do
+  partition = Controls::Partition.example
+
+  stream = Controls::Put.(instances: 2, partition: partition)
+
+  batch = []
+
+  Read.(stream.name, partition: partition, batch_size: 1) do |event_data|
+    batch << event_data
+  end
+
+  test "Reads batches of events" do
+    assert(batch.length == 2)
+  end
+end
