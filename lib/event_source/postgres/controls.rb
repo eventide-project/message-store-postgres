004c29b2d8116f633b3ffca07a7c8dfeeff62e1a
Scott Bellware <sbellware@gmail.com>
2016-10-28 22:20:53 -0500

    Superfluous control namespace aliases are removed

--- lib/event_source/postgres/controls.rb
+++ lib/event_source/postgres/controls.rb
@@ -5,9 +5,6 @@ require 'event_source/postgres/controls/stream_name'
 require 'event_source/postgres/controls/stream'
 
 require 'event_source/postgres/controls/event_data'
-require 'event_source/postgres/controls/event_data/hash'
-require 'event_source/postgres/controls/event_data/metadata'
-require 'event_source/postgres/controls/event_data/write'
 
 require 'event_source/postgres/controls/partition'
 
