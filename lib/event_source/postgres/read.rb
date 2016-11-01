c73751e41b087cde86cc88ce38a1a83d0a4b5660
Scott Bellware <sbellware@gmail.com>
2016-11-01 17:02:32 -0500

    Read base class in now a module

--- lib/event_source/postgres/read.rb
+++ lib/event_source/postgres/read.rb
@@ -1,7 +1,9 @@
 module EventSource
   module Postgres
-    class Read < EventSource::Read
-      def build_get(stream, batch_size: nil, precedence: nil, partition: nil, session: nil)
+    class Read
+      include EventSource::Read
+
+      def self.build_get(stream, batch_size: nil, precedence: nil, partition: nil, session: nil)
         Get.build(stream, batch_size: batch_size, precedence: precedence, partition: partition, session: session)
       end
     end
