83c95964cf8dc28b732ca3d627f602c5ab0559ff
Scott Bellware <sbellware@gmail.com>
2016-11-05 01:03:34 -0500

    Test for writing batch with expected version

--- test/automated/write/batch/expected_version.rb
+++ test/automated/write/batch/expected_version.rb
@@ -2,24 +2,26 @@ require_relative '../../automated_init'
 
 context "Write" do
   context "Batch" do
-    stream_name = Controls::StreamName.example
+    context "Expected Version" do
+      stream_name = Controls::StreamName.example
 
-    write_event = Controls::EventData::Write.example
-    written_position = Write.(write_event, stream_name)
+      write_event = Controls::EventData::Write.example
+      written_position = Write.(write_event, stream_name)
 
-    write_event_1 = Controls::EventData::Write.example(data: { attribute: 'value_1' })
-    write_event_2 = Controls::EventData::Write.example(data: { attribute: 'value_2' })
+      write_event_1 = Controls::EventData::Write.example(data: { attribute: 'value_1' })
+      write_event_2 = Controls::EventData::Write.example(data: { attribute: 'value_2' })
 
-    batch = [write_event_1, write_event_2]
+      batch = [write_event_1, write_event_2]
 
-    Write.(batch, stream_name, expected_version: written_position)
+      Write.(batch, stream_name, expected_version: written_position)
 
-    context "Individual Events are Written" do
-      2.times do |i|
-        read_event = Get.(stream_name, position: i + written_position, batch_size: 1).first
+      context "Individual Events are Written" do
+        2.times do |i|
+          read_event = Get.(stream_name, position: i + written_position, batch_size: 1).first
 
-        test "Event #{i + 1}" do
-          refute(read_event.nil?)
+          test "Event #{i + 1}" do
+            refute(read_event.nil?)
+          end
         end
       end
     end
