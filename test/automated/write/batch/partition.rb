242e4d0b8241278ab47eb42e68c2e838c157ac0f
Scott Bellware <sbellware@gmail.com>
2016-11-17 16:31:41 -0600

    Write to partition is tested

---
+++ test/automated/write/batch/partition.rb
@@ -0,0 +1,28 @@
+require_relative '../../automated_init'
+
+context "Write" do
+  context "Batch" do
+    context "Partition" do
+      stream_name = Controls::StreamName.example
+
+      write_event_1 = Controls::EventData::Write.example(data: { attribute: 'value_1' })
+      write_event_2 = Controls::EventData::Write.example(data: { attribute: 'value_2' })
+
+      batch = [write_event_1, write_event_2]
+
+      partition = Controls::Partition.example
+
+      Write.(batch, stream_name, partition: partition)
+
+      context "Individual Events are Written" do
+        2.times do |i|
+          read_event = Get.(stream_name, partition: partition, position: i, batch_size: 1).first
+
+          test "Event #{i + 1}" do
+            refute(read_event.nil?)
+          end
+        end
+      end
+    end
+  end
+end
