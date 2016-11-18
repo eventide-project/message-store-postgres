4cbe183870c71be295f0fb71b892de17c2ef4819
Scott Bellware <sbellware@gmail.com>
2016-11-18 17:28:02 -0600

    Partition control

--- lib/event_source/postgres/controls/partition.rb
+++ lib/event_source/postgres/controls/partition.rb
@@ -1,11 +1,7 @@
 module EventSource
   module Postgres
     module Controls
-      module Partition
-        def self.example
-          'other_events'
-        end
-      end
+      Partition = EventSource::Controls::Partition
     end
   end
 end
