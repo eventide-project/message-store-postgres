172f536a308f122eb1507c507439088e5c2b6a44
Scott Bellware <sbellware@gmail.com>
2016-11-18 22:20:56 -0600

    Batch partition write test

--- test/automated/write/batch/partition.rb
+++ test/automated/write/batch/partition.rb
@@ -5,14 +5,15 @@ context "Write" do
     context "Partition" do
       stream_name = Controls::StreamName.example
 
-      write_event_1 = Controls::EventData::Write.example
-      write_event_2 = Controls::EventData::Write.example
+      write_event_1 = Controls::EventData::Write.example(data: { attribute: 'value_1' })
+      write_event_2 = Controls::EventData::Write.example(data: { attribute: 'value_2' })
 
       batch = [write_event_1, write_event_2]
 
       partition = Controls::Partition.example
 
       last_written_position = Write.(batch, stream_name, partition: partition)
+
 
       test "Last written position" do
         assert(last_written_position == 1)
@@ -23,7 +24,7 @@ context "Write" do
           read_event = Get.(stream_name, partition: partition, position: i, batch_size: 1).first
 
           test "Event #{i + 1}" do
-            refute(read_event.nil?)
+            assert(read_event.data[:attribute] == "value_#{i + 1}")
           end
         end
       end
