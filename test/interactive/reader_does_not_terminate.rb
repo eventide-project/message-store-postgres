57093b23ee893a10a982b63d9a185ceb33c67dd3
Scott Bellware <sbellware@gmail.com>
2016-11-26 16:17:14 -0600

    Read's cycle parameters are prefixed with cycle

--- test/interactive/reader_does_not_terminate.rb
+++ test/interactive/reader_does_not_terminate.rb
@@ -2,4 +2,4 @@ require_relative '../test_init'
 
 stream_name = Controls::StreamName.example
 
-Read.(stream_name, batch_size: 1, delay_milliseconds: 200) { |event_data| }
+Read.(stream_name, batch_size: 1, cycle_delay_milliseconds: 200) { |event_data| }
