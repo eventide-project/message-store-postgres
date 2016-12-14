e64eedaf377e0850ddf25610666daea85a37190c
Scott Bellware <sbellware@gmail.com>
2016-12-14 14:55:28 -0600

    Test for connect on first use

--- test/automated/session/session.rb
+++ test/automated/session/session.rb
@@ -2,6 +2,14 @@ require_relative '../automated_init'
 
 context "Session" do
   test "On First Use" do
-    test "Connects"
+    session = Session.build
+
+    refute(session.connected?)
+
+    test "Connects" do
+      refute proc { session.execute('SELECT 1;') } do
+        raises_error?
+      end
+    end
   end
 end
