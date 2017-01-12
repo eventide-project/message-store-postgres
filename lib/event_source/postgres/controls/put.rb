a93cc10fc58e99c786b0ff678dbd937d5d77fbdf
Scott Bellware <sbellware@gmail.com>
2017-01-12 15:12:53 -0600

    Control assigns new ID for each event written

--- lib/event_source/postgres/controls/put.rb
+++ lib/event_source/postgres/controls/put.rb
@@ -5,10 +5,17 @@ module EventSource
         def self.call(instances: nil, stream_name: nil, event: nil, category: nil)
           instances ||= 1
           stream_name ||= StreamName.example(category: category)
+
+          event_specified = !event.nil?
+
           event ||= EventData::Write.example
 
           instances.times do
             EventSource::Postgres::Put.(event, stream_name)
+
+            unless event_specified
+              event.id = EventData::Write.id
+            end
           end
 
           stream_name
