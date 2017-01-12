0da2ebaf4f3d43830c83afaa5930ab298aa0b0d8
Scott Bellware <sbellware@gmail.com>
2017-01-12 16:03:37 -0600

    Event ID defaults to a random UUID

--- database/table/events-table.sql
+++ database/table/events-table.sql
@@ -3,8 +3,7 @@
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."events";
 CREATE TABLE "public"."events" (
-  -- "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-  "id" UUID NOT NULL,
+  "id" UUID NOT NULL DEFAULT gen_random_uuid(),
   "stream_name" varchar(255) NOT NULL COLLATE "default",
   "position" int4 NOT NULL,
   "type" varchar(255) NOT NULL COLLATE "default",
