7641b960621b3b8e0c7f9453c293895a8640d200
Scott Bellware <sbellware@gmail.com>
2017-02-04 00:38:36 -0600

    Read has batch size property

--- lib/event_source/postgres/read.rb
+++ lib/event_source/postgres/read.rb
@@ -3,7 +3,7 @@ module EventSource
     class Read
       include EventSource::Read
 
-      def configure(batch_size: nil, session: nil)
+      def configure(session: nil)
         Iterator.configure(self, stream_name, position: position)
         Get.configure(self.iterator, batch_size: batch_size, session: session)
       end
