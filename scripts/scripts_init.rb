244f8e8259fb36749cf4ec32fe6269cc4130554e
Scott Bellware <sbellware@gmail.com>
2017-03-31 15:29:06 -0500

    Inactive code is removed

--- scripts/scripts_init.rb
+++ scripts/scripts_init.rb
@@ -4,7 +4,6 @@ ENV['LOG_LEVEL'] ||= '_min'
 puts RUBY_DESCRIPTION
 
 init_file = File.expand_path('../init.rb', __dir__)
-# if File.exist?('init.rb')
 if File.exist?(init_file)
   require_relative '../init'
 end
