2ef7436c2363a276c372962160056d19ecc8fc85
Scott Bellware <sbellware@gmail.com>
2017-05-24 23:09:31 -0500

    Gem installation is brought up to current standards

--- install-gems.sh
+++ install-gems.sh
@@ -2,25 +2,6 @@
 
 set -e
 
-echo
-echo 'Installing local gems'
-echo '= = ='
-
-source ./set-local-gem-path.sh
-
-echo
-echo 'Removing gem files'
-echo '- - -'
-! rm -v *.gem
-
-echo
-echo 'Building gems'
-echo '- - -'
-for gemspec in *.gemspec; do
-  echo "- $gemspec"
-  gem build $gemspec
-done
-
 if [ -z ${POSTURE+x} ]; then
   echo "(POSTURE is not set. Using \"operational\" by default.)"
   posture="operational"
@@ -30,19 +11,17 @@ fi
 
 echo
 echo "Installing gems locally (posture: $posture)"
-echo '- - -'
-for gem in *.gem; do
-  echo "($gem)"
-  cmd="gem install $gem --install-dir ./gems"
-
-  if [ operational != "$posture" ]; then
-    cmd="$cmd --development"
-  fi
-
-  echo $cmd
-  ($cmd) || exit 1
-done
-
 echo '= = ='
+
+cmd="bundle install --standalone --path=./gems"
+
+if [ operational == "$posture" ]; then
+  cmd="$cmd --without=development"
+fi
+
+echo $cmd
+($cmd)
+
+echo '- - -'
 echo '(done)'
 echo
