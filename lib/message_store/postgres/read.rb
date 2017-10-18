3b0a02927f836e40e022ff2e8c00f228b225d545
ntl <nathanladd+github@gmail.com>
2017-10-18 14:15:56 -0500

    Read configures Get with optional condition

--- lib/message_store/postgres/read.rb
+++ lib/message_store/postgres/read.rb
@@ -3,9 +3,9 @@ module MessageStore
     class Read
       include MessageStore::Read
 
-      def configure(session: nil)
+      def configure(session: nil, condition: nil)
         Iterator.configure(self, stream_name, position: position)
-        Get.configure(self.iterator, batch_size: batch_size, session: session)
+        Get.configure(self.iterator, batch_size: batch_size, condition: condition, session: session)
       end
     end
   end
---
+++ test/automated/read/condition.rb
@@ -0,0 +1,19 @@
+require_relative '../automated_init'
+
+context "Read" do
+  context "Condition" do
+    stream_name = Controls::Put.(instances: 3)
+
+    condition = 'position = 0'
+
+    message_count = 0
+
+    Read.(stream_name, condition: condition) do
+      message_count += 1
+    end
+
+    test "Reads messages that meet condition" do
+      assert(message_count == 1)
+    end
+  end
+end
