655b3f780d89082b5a66d75f067d0360da9b0ca7
Scott Bellware <sbellware@gmail.com>
2018-05-30 23:11:26 -0500

    Write benchmark is replaced with put benchmark

--- test/benchmark/put.rb
+++ test/benchmark/put.rb
@@ -1,18 +1,18 @@
-require_relative '../benchmark_init'
+require_relative 'benchmark_init'
 
 defaults = Benchmark::Defaults.build
 
 puts
-puts 'Write Benchmark'
+puts 'Put Benchmark'
 puts '- - -'
 
 list = Controls::MessageData::Write::List.get(instances: defaults.total_cycles)
 
-write = Write.build
+put = Put.build
 
 result = Diagnostics::Sample.(defaults.cycles, warmup_cycles: defaults.warmup_cycles, gc: defaults.gc) do |i|
   entry = list[i]
-  write.(entry.message_data, entry.stream_name)
+  put.(entry.message_data, entry.stream_name)
 end
 
 puts result
