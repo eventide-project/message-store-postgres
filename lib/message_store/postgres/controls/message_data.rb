0114989a587fe817aced00d7b2a8ab018f7fea7d
Scott Bellware <sbellware@gmail.com>
2018-05-30 23:06:15 -0500

    Message data used in write benchmark is included in the controls

--- lib/message_store/postgres/controls/message_data.rb
+++ lib/message_store/postgres/controls/message_data.rb
@@ -4,6 +4,27 @@ module MessageStore
       MessageData = MessageStore::Controls::MessageData
 
       module MessageData
+        module Write
+          module List
+            Entry = Struct.new(:stream_name, :message_data)
+
+            def self.get(instances: nil)
+              instances ||= 1
+
+              stream_name = StreamName.example
+
+              list = []
+              instances.times do
+                stream_name = Controls::StreamName.example
+                write_message = Controls::MessageData::Write.example
+
+                list << Entry.new(stream_name, write_message)
+              end
+
+              list
+            end
+          end
+        end
       end
     end
   end
