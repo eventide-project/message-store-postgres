621d2ee7a254bf25d85cc7b31b718aba12979efa
Scott Bellware <sbellware@gmail.com>
2018-05-30 22:04:58 -0500

    Write benchmark structure is stable.
    Next: moves writes to controls

--- lib/message_store/postgres/controls/message_data.rb
+++ lib/message_store/postgres/controls/message_data.rb
@@ -2,6 +2,9 @@ module MessageStore
   module Postgres
     module Controls
       MessageData = MessageStore::Controls::MessageData
+
+      module MessageData
+      end
     end
   end
 end
--- test/benchmark/write/write.rb
+++ test/benchmark/write/write.rb
@@ -1,26 +1,27 @@
-require_relative 'benchmark_init'
-require 'benchmark'
+require_relative '../benchmark_init'
 
-warmup_cycles = 10
-test_cycles = 10_000
-cycles = warmup_cycles + test_cycles
+defaults = Benchmark::Defaults.build
+
+puts
+puts 'Write Benchmark'
+puts '- - -'
 
 Entry = Struct.new(:stream_name, :message_data)
 
 entries = []
-cycles.times do
+defaults.total_cycles.times do
   stream_name = Controls::StreamName.example
   write_message = Controls::MessageData::Write.example
 
   entries << Entry.new(stream_name, write_message)
 end
 
-session = Session.build
-write = Write.build(session: session)
+write = Write.build
 
-result = Diagnostics::Sample.(test_cycles, warmup_cycles: warmup_cycles, gc: true) do |i|
+result = Diagnostics::Sample.(defaults.cycles, warmup_cycles: defaults.warmup_cycles, gc: defaults.gc) do |i|
   entry = entries[i]
   write.(entry.message_data, entry.stream_name)
 end
 
 puts result
+puts
