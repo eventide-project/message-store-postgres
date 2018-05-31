72ccd0b91ca9662ea27246f0ab5148d16aa08199
Scott Bellware <sbellware@gmail.com>
2018-05-30 23:06:15 -0500

    Message data used in write benchmark is included in the controls

--- test/benchmark/write/write.rb
+++ test/benchmark/write/write.rb
@@ -6,20 +6,12 @@ puts
 puts 'Write Benchmark'
 puts '- - -'
 
-Entry = Struct.new(:stream_name, :message_data)
-
-entries = []
-defaults.total_cycles.times do
-  stream_name = Controls::StreamName.example
-  write_message = Controls::MessageData::Write.example
-
-  entries << Entry.new(stream_name, write_message)
-end
+list = Controls::MessageData::Write::List.get(instances: defaults.total_cycles)
 
 write = Write.build
 
 result = Diagnostics::Sample.(defaults.cycles, warmup_cycles: defaults.warmup_cycles, gc: defaults.gc) do |i|
-  entry = entries[i]
+  entry = list[i]
   write.(entry.message_data, entry.stream_name)
 end
 
