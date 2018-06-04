79fe7c2dd2e6122ae11b0b571d564ae20ea421f0
Scott Bellware <sbellware@gmail.com>
2018-06-03 22:59:36 -0500

    Interactive test script for concurrent writes

--- test/interactive/interactive_init.rb
+++ test/interactive/interactive_init.rb
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 ENV['LOG_LEVEL'] ||= 'info'
 ENV['LOG_TAGS'] ||= '_untagged,message_store_postgres,-data'
 
-=======
->>>>>>> Interactive test script for concurrent writes
 require_relative '../test_init'
