5303b3a085bea07247c1bc7c0fac7d7f7e9216be
Scott Bellware <sbellware@gmail.com>
2018-06-04 18:11:02 -0500

    Advisory locks baseline implementation

--- test/interactive/write.rb
+++ test/interactive/write.rb
@@ -1,13 +1,16 @@
 require_relative 'interactive_init'
 
-message_data = Controls::MessageData::Write.example
 stream_name = 'testWriteIsolation'
+
+message_data_1 = Controls::MessageData::Write.example(data: { attribute: 1 })
+message_data_2 = Controls::MessageData::Write.example(data: { attribute: 2 })
+batch = [message_data_1, message_data_2]
 
 write = Write.build
 
 i = 0
 loop do
-  position = write.(message_data, stream_name)
+  position = write.(batch, stream_name)
   puts "i: #{i}, p: #{position}"
   i += 1
 end
