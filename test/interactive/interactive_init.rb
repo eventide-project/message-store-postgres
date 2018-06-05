7efac7f1227ca418a443b4f6b7c28eeaa54ec6e0
Scott Bellware <sbellware@gmail.com>
2018-06-05 09:27:15 -0500

    Merge conflict is resolved

--- test/interactive/interactive_init.rb
+++ test/interactive/interactive_init.rb
@@ -1,4 +1,7 @@
+<<<<<<< HEAD
 ENV['LOG_LEVEL'] ||= 'info'
 ENV['LOG_TAGS'] ||= '_untagged,message_store_postgres,-data'
 
+=======
+>>>>>>> Interactive test script for concurrent writes
 require_relative '../test_init'
