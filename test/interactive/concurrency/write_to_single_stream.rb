228cde47d0213ed202d8d29f5f1156c5f0ce482d
Scott Bellware <sbellware@gmail.com>
2018-06-05 12:36:52 -0500

    Concurrent write actor is namespaced

--- test/interactive/concurrency/write_to_single_stream.rb
+++ test/interactive/concurrency/write_to_single_stream.rb
@@ -2,35 +2,41 @@ require_relative 'concurrency_init'
 
 require 'actor'
 
-class ExampleProcess
-  include Actor
-  include Log::Dependency
+module Test
+  module Interactive
+    module Concurrency
+      class Write
+        include Actor
+        include Log::Dependency
 
-  attr_reader :stream_name
+        attr_reader :stream_name
 
-  def initialize(stream_name)
-    @stream_name = stream_name
-  end
+        def initialize(stream_name)
+          @stream_name = stream_name
+        end
 
-  handle :start do
-    :write_message
-  end
+        handle :start do
+          :write_message
+        end
 
-  handle :write_message do
-    message_data_1 = MessageStore::Postgres::Controls::MessageData::Write.example(data: { actor: object_id })
-    message_data_2 = MessageStore::Postgres::Controls::MessageData::Write.example(data: { actor: object_id })
-    batch = [message_data_1, message_data_2]
+        handle :write_message do
+          message_data_1 = MessageStore::Postgres::Controls::MessageData::Write.example(data: { actor: object_id })
+          message_data_2 = MessageStore::Postgres::Controls::MessageData::Write.example(data: { actor: object_id })
+          batch = [message_data_1, message_data_2]
 
-    position = MessageStore::Postgres::Write.(batch, stream_name)
+          position = MessageStore::Postgres::Write.(batch, stream_name)
 
-    logger.info(tag: :actor) { "Wrote message data (Object ID: #{object_id}, Position: #{position}, Message Type: #{message_data_1.type.inspect}, Stream Name: #{stream_name.inspect})" }
+          logger.info(tag: :actor) { "Wrote message data (Object ID: #{object_id}, Position: #{position}, Message Type: #{message_data_1.type.inspect}, Stream Name: #{stream_name.inspect})" }
 
-    :write_message
+          :write_message
+        end
+      end
+    end
   end
 end
 
-random = SecureRandom.hex[0..5]
-stream_name = Controls::StreamName.example(category: "testConcurrentWrite-#{random}", randomize_category: false)
+random = SecureRandom.hex[0..3]
+stream_name = Controls::StreamName.example(category: "testConcurrentWrite_#{random}", randomize_category: false, id: :none)
 
 number_of_actors = Test::Interactive::Concurrency::Defaults.actors
 
@@ -41,6 +47,6 @@ puts
 
 Actor::Supervisor.start do
   number_of_actors.times do
-    ExampleProcess.start(stream_name)
+    Test::Interactive::Concurrency::Write.start(stream_name)
   end
 end
