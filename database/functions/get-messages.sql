2b65234269cfb8f3b28c47c2023aee09762bdf64
Scott Bellware <sbellware@gmail.com>
2018-06-06 14:14:44 -0500

    Where clause is dynamic and optional

--- database/functions/get-messages.sql
+++ database/functions/get-messages.sql
@@ -1,10 +1,18 @@
-CREATE OR REPLACE FUNCTION get_messages()
+CREATE OR REPLACE FUNCTION get_messages(
+  condition varchar DEFAULT NULL
+)
 RETURNS SETOF messages
 AS $$
 DECLARE
   command text;
 BEGIN
   command := 'SELECT * FROM messages';
+
+  if condition is not null then
+    command := command || ' WHERE %s';
+    command := format(command, condition);
+  end if;
+
   RETURN QUERY EXECUTE command;
 END;
 $$ LANGUAGE plpgsql
