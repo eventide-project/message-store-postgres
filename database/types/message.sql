7d3f9d665e3a3214b0b8285d71f1f5a7a4df3db1
Scott Bellware <sbellware@gmail.com>
2018-06-08 01:28:01 -0500

    Script installs server functions

--- database/types/message.sql
+++ database/types/message.sql
@@ -1,10 +1,15 @@
-CREATE TYPE message AS (
-  id varchar,
-  stream_name varchar,
-  type varchar,
-  position bigint,
-  global_position bigint,
-  data varchar,
-  metadata varchar,
-  time timestamp
-);
+DO $$
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'message') THEN
+    CREATE TYPE message AS (
+      id varchar,
+      stream_name varchar,
+      type varchar,
+      position bigint,
+      global_position bigint,
+      data varchar,
+      metadata varchar,
+      time timestamp
+    );
+  END IF;
+END$$;
