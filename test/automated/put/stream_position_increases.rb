0f413c183bd59d724ba993b48209386f4dc6a4b3
Scott Bellware <sbellware@gmail.com>
2018-06-25 10:29:06 -0500

    Test does not accidentally cause uniqueness violation by writing the same message with the same ID more than once

--- test/automated/put/stream_position_increases.rb
+++ test/automated/put/stream_position_increases.rb
@@ -3,10 +3,12 @@ require_relative '../automated_init'
 context "Put" do
   context "Stream Position Increases with Subsequent Writes" do
     stream_name = Controls::StreamName.example
-    write_message = Controls::MessageData::Write.example
 
-    position_1 = Put.(write_message, stream_name)
-    position_2 = Put.(write_message, stream_name)
+    write_message_1 = Controls::MessageData::Write.example
+    write_message_2 = Controls::MessageData::Write.example
+
+    position_1 = Put.(write_message_1, stream_name)
+    position_2 = Put.(write_message_2, stream_name)
 
     test "First version is one less than the second version" do
       assert(position_1 + 1 == position_2)
