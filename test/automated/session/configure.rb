4032e79344e24398c77b0a523d5937b37a39ed30
Scott Bellware <sbellware@gmail.com>
2018-10-02 19:26:52 -0500

    Session and settings test coverage is increased

---
+++ test/automated/session/configure.rb
@@ -0,0 +1,47 @@
+require_relative '../automated_init'
+
+context "Session" do
+  context "Configure" do
+    context "Session is specified" do
+      receiver = OpenStruct.new
+      settings = Object.new
+      session = Object.new
+
+      Session.configure(receiver, session: session)
+
+      test "Specified session is used" do
+        assert(receiver.session == session)
+      end
+    end
+
+    context "Settings is specified" do
+      receiver = OpenStruct.new
+
+      user = SecureRandom.hex
+
+      settings_data = {
+        user: user
+      }
+
+      settings = MessageStore::Postgres::Settings.build(settings_data)
+
+      Session.configure(receiver, settings: settings)
+
+      test "Session is built from settings" do
+        assert(receiver.session.user == settings.get('user'))
+      end
+    end
+
+    context "Specifying both the session and settings" do
+      receiver = OpenStruct.new
+      settings = Object.new
+      session = Object.new
+
+      test "Is an error" do
+        assert proc { Session.configure(receiver, settings: settings, session: session) } do
+          raises_error? Session::Error
+        end
+      end
+    end
+  end
+end
