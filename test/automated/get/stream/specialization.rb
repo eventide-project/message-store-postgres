e56f62e7fb93f6ba58d76a0e1f681b8f17bcfefe
Scott Bellware <sbellware@gmail.com>
2019-11-28 16:58:44 -0600

    Specializations are tested

--- test/automated/get/stream/specialization.rb
+++ test/automated/get/stream/specialization.rb
@@ -5,10 +5,41 @@ context "Get" do
     context "Specialization" do
       stream_name = Controls::StreamName.example
 
-      get = Get.build(stream_name)
+      batch_size = 1
+      correlation = 'someCorrelation'
+      consumer_group_member = 0
+      consumer_group_size = 1
+      condition = 'global_position >= 1'
+      session = Session.build
 
-      test "Get::Stream" do
-        assert(get.instance_of?(Get::Stream))
+      get = Get.build(stream_name, batch_size: batch_size, correlation: correlation, condition: condition, session: session)
+
+      context "Type" do
+        test "Get::Stream" do
+          assert(get.instance_of?(Get::Stream))
+        end
+      end
+
+      context "Attributes" do
+        test "stream_name" do
+          assert(get.stream_name == stream_name)
+        end
+
+        test "batch_size" do
+          assert(get.batch_size == batch_size)
+        end
+
+        test "correlation" do
+          assert(get.correlation == correlation)
+        end
+
+        test "condition" do
+          assert(get.condition == condition)
+        end
+
+        test "session" do
+          assert(get.session == session)
+        end
       end
     end
   end
