9c9ea398d8d1801a7cccceb9eb4079bf374a8004
Scott Bellware <sbellware@gmail.com>
2019-12-19 12:50:09 -0600

    Category benchmark is operational

--- test/benchmark/get_category.rb
+++ test/benchmark/get_category.rb
@@ -3,32 +3,37 @@ require_relative 'benchmark_init'
 defaults = Test::Benchmark::Defaults.build
 
 puts
-puts "Running benchmark (#{__FILE__})..."
+puts "Get Category Benchmark (#{__FILE__})"
 puts
 
+puts "» defaults"
+puts defaults.to_s
 
 total_cycles = defaults.total_cycles
+puts
 
-puts "- constructing #{total_cycles} entries"
+puts "» constructing #{total_cycles} entries"
 list = Controls::MessageData::Write::List.get(instances: total_cycles)
-
-##
-pp list
 
 put = Put.build
 
-puts "- writing entries"
+puts "» writing entries"
 list.each do |entry|
   stream_name = defaults.stream_name || entry.stream_name
+
+  if defaults.verbose
+    puts "Stream: #{stream_name}"
+  end
+
   put.(entry.message_data, stream_name)
 end
 
 category = Controls::Category.example
 
-puts "- constructing Get::Category for #{category}"
-get = Get::Category.build(category)
+puts "» constructing Get::Category"
+get = Get::Category.build('')
 
-puts "- executing and sampling #{total_cycles} cycles"
+puts "» executing and sampling #{total_cycles} cycles"
 result = Diagnostics::Sample.(defaults.cycles, warmup_cycles: defaults.warmup_cycles, gc: defaults.gc) do |i|
   entry = list[i]
   stream_name = MessageStore::StreamName.get_category(defaults.stream_name || entry.stream_name)
@@ -40,7 +45,7 @@ puts "- getting #{stream_name}"
 end
 
 puts
-filename = Benchmark::RecordResult.('Get Benchmark', result)
+filename = Benchmark::RecordResult.('Get Category Benchmark', result)
 puts
 puts filename
 puts
--- test/benchmark/get_stream.rb
+++ test/benchmark/get_stream.rb
@@ -3,20 +3,27 @@ require_relative 'benchmark_init'
 defaults = Test::Benchmark::Defaults.build
 
 puts
-puts "Running benchmark (#{__FILE__})..."
+puts "Get Stream Benchmark (#{__FILE__})"
 puts
 
+puts defaults.to_s
+puts
+
+puts "» constructing #{total_cycles} entries"
 list = Controls::MessageData::Write::List.get(instances: defaults.total_cycles)
 
 put = Put.build
 
+puts "» writing entries"
 list.each do |entry|
   stream_name = defaults.stream_name || entry.stream_name
   put.(entry.message_data, stream_name)
 end
 
-get = Get.build(Controls::StreamName.example)
+puts "» constructing Get::Stream"
+get = Get::Stream.build(Controls::StreamName.example)
 
+puts "» executing and sampling #{total_cycles} cycles"
 result = Diagnostics::Sample.(defaults.cycles, warmup_cycles: defaults.warmup_cycles, gc: defaults.gc) do |i|
   entry = list[i]
   stream_name = defaults.stream_name || entry.stream_name
@@ -24,7 +31,7 @@ result = Diagnostics::Sample.(defaults.cycles, warmup_cycles: defaults.warmup_cy
 end
 
 puts
-filename = Benchmark::RecordResult.('Get Benchmark', result)
+filename = Benchmark::RecordResult.('Get Stream Benchmark', result)
 puts
 puts filename
 puts
